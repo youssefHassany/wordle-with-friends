e168fb46055ea41f0bce7446c968ea4ad9cc2df7
unknown <youssefhassany22@gmail.com>
2024-03-19 02:12:18 +0200

    fixing game page not found in production

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -2,16 +2,28 @@ import Board from "./Board";
 import Keyboard from "./Keyboard";
 import { useParams } from "react-router-dom";
 import { useDecrypt } from "../../hooks/useDecrypt";
+import { useEffect, useState } from "react";
 
 const Game = () => {
   const { encryptedVariable } = useParams();
-  const decryptedValue = useDecrypt(encryptedVariable?.slice(1));
-  let rightAns = decryptedValue;
+  console.log(encryptedVariable);
+  const [rightAns, setRightAnswer] = useState<string | undefined>();
+
+  useEffect(() => {
+    const decrypted = useDecrypt(encryptedVariable);
+    setRightAnswer(decrypted);
+  }, []);
 
   return (
     <main>
-      <Board rightWord={rightAns} />
-      <Keyboard rightWord={rightAns} />
+      {rightAns ? (
+        <>
+          <Board rightWord={rightAns} />
+          <Keyboard rightWord={rightAns} />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </main>
   );
 };
